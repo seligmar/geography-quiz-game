830036c2a9eb006e1580151a0e301a265beb5ec6
github username <github email address>
2019-09-09 13:56:47 +0100

    initialized routes

--- ArchitectureTrivia/config/routes.rb
+++ ArchitectureTrivia/config/routes.rb
@@ -1,3 +1,9 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/questions", to: "question#index", as: "questions" 
+  
+  get "/users", to: "user#index", as: "users"
+  get "/users", to: "user#new", as: "new_user" 
+  post "/users", to: "user#create" 
+
 end
