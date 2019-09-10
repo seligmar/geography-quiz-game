7fa1fd3fb90436fa9f6f041e1dc76ccb5dc55b19
saazim1123 <saazim1123@gmail.com>
2019-09-10 13:53:52 +0100

    do not use these changes

--- ArchitectureTrivia/app/assets/javascripts/mod2js.js
+++ ArchitectureTrivia/app/assets/javascripts/mod2js.js
@@ -5,21 +5,30 @@ const qBox = document.querySelector("#qs-go-here")
 const index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 usernameForm.addEventListener("submit", e => {removeStartBox(e), pickRandom()})
+
 function removeStartBox(e) {
+   debugger
    e.preventDefault()
    document.querySelector(".start-screen").style.display = "none";
 }
+
+usernameForm.addEventListener("submit", e => {removeStartBox(e), pickRandom()})
+
+function removeStartBox(e) {
+  e.preventDefault()
+  document.querySelector(".start-screen").style.display = "none";
+}
 function pickRandom() {
-   if (index.length === 0) {
-       endGame()}
-   let rand = index[Math.floor(Math.random() * index.length)];
-   index.splice(index[rand], 1)
-   fetchRandom(rand)
+  if (index.length === 0) {
+      endGame()}
+  let rand = index[Math.floor(Math.random() * index.length)];
+  index.splice(index[rand], 1)
+  fetchRandom(rand)
 }
 function fetchRandom(n) {
-   return fetch(`http://localhost:3000/questions/${n}`)
-   .then(resp => resp.json())
-   .then(showQuestion)
+  return fetch(`http://localhost:3000/questions/${n}`)
+  .then(resp => resp.json())
+  .then(showQuestion)
 }
 function showQuestion(question) {
    let qBoxDiv = document.createElement('div')
@@ -29,15 +38,14 @@ function showQuestion(question) {
    let btn3 = document.createElement('button')
    let btn4 = document.createElement('button')
    qBoxDiv.className = "question-box"
-   imageBox.innerHTML = <img class = "question-box" src="${question.img}"/>
+   imageBox.innerHTML = `<img class = "question-box" src="${question.img}"/>`
    qBoxDiv.append(imageBox)
-   btn1.innerText = ${question.answer1}
-   btn2.innerText = ${question.correct_answer}
-   btn3.innerText = ${question.answer3}
-   btn4.innerText = ${question.answer2}
+   btn1.innerText = `${question.answer1}`
+   btn2.innerText = `${question.correct_answer}`
+   btn3.innerText = `${question.answer3}`
+   btn4.innerText = `${question.answer2}`
    qBox.append(qBoxDiv)
 }
-
 function endGame() {
-   console.log("OK")
+  console.log("OK")
 }
