e289c1933c5542285b69bc8ff2b0f8b507a732f2
saazim1123 <saazim1123@gmail.com>
2019-09-10 13:17:51 +0100

    Will work on rendering the questions

--- ArchitectureTrivia/mod2js.js
+++ ArchitectureTrivia/mod2js.js
@@ -1,7 +1,22 @@
-var indecies = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-function pickRandom(indecies) {
-   let rand = indecies[Math.floor(Math.random() * indecies.length)];
-   indecies = indecies.splice(indecies[rand], 1)
+const usernameForm = document.querySelector("#usernameForm")
+const startBtn = document.querySelector("#button")
+const ol = document.createElement('ol')
+const qBox = document.querySelector("#qs-go-here")
+const index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const inputField = document.querySelector('#username')
+
+
+usernameForm.addEventListener("submit", e => {removeStartBox(e), pickRandom()})
+function removeStartBox(e) {
+   e.preventDefault()
+   document.querySelector(".start-screen").style.display = "none";
+}
+function pickRandom() {
+   if (index.length === 0) {
+       endGame()}
+   let rand = index[Math.floor(Math.random() * index.length)];
+   index.splice(index[rand], 1)
    fetchRandom(rand)
 }
 function fetchRandom(n) {
@@ -9,5 +24,28 @@ function fetchRandom(n) {
    .then(resp => resp.json())
    .then(showQuestion)
 }
-function showQuestion() {
+function showQuestion(question) {
+   let qBoxDiv = document.createElement('div')
+   let imageBox = document.createElement('div')
+   let btn1 = document.createElement('button')
+   let btn2 = document.createElement('button')
+   let btn3 = document.createElement('button')
+   let btn4 = document.createElement('button')
+   qBoxDiv.className = "question-box"
+   imageBox.innerHTML = <img class = "question-box" src="${question.img}"/>
+   qBoxDiv.append(imageBox)
+   btn1.innerText = ${question.answer1}
+   btn2.innerText = ${question.correct_answer}
+   btn3.innerText = ${question.answer3}
+   btn4.innerText = ${question.answer2}
+   qBox.append(qBoxDiv)
 }
+
+
+
+inputField.addEventListener("click", e =>{
+    inputField.value = ""
+})
+
+
+
