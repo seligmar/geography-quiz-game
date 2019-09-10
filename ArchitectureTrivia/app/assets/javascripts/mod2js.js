f22ebfa4055f813a6f934e1e6378493db6600fad
github username <github email address>
2019-09-10 12:31:00 +0100

    fixed seed data all js routes work thus far

--- ArchitectureTrivia/app/assets/javascripts/mod2js.js
+++ ArchitectureTrivia/app/assets/javascripts/mod2js.js
@@ -1,10 +1,23 @@
-//alert("I am an alert box")
 
-let indecies = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+const usernameForm = document.querySelector("#usernameForm")
+const startBtn = document.querySelector("#button")
+const ol = document.createElement('ol')
+
+const qBox = document.querySelector("#qs-go-here")
+
+const index = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+usernameForm.addEventListener("submit", e => {removeStartBox(e), pickRandom()})
+
+function removeStartBox(e) {
+    e.preventDefault()
+    document.querySelector(".start-screen").style.display = "none";
+}
 
 function pickRandom() {
-    let rand = indecies[Math.floor(Math.random() * indecies.length)];
-    indecies.splice(indecies[rand], 1)
+    let rand = index[Math.floor(Math.random() * index.length)];
+    index.splice(index[rand], 1)
     fetchRandom(rand)
 }
 
@@ -14,6 +27,16 @@ function fetchRandom(n) {
     .then(showQuestion)
 }
 
-function showQuestion() {
-
+function showQuestion(question) {
+    let qBoxDiv = document.createElement('div')
+    let imageBox = document.createElement('div')
+    qBoxDiv.className = "question-box"
+    imageBox.innerHTML = `<img class = "question-box" src="${question.img}"/>`
+    debugger
+    qBoxDiv.append(imageBox)
+     qBoxDiv.innerHTML = `
+        <p>${question.answer1}</p>
+     `
+    qBox.append(qBoxDiv)
 }
+
