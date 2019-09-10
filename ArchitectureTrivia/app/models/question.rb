4b794edbf0562c89ed18b048189cb4c576f4d8a0
github username <github email address>
2019-09-10 10:21:49 +0100

    added fetch functionality randomized indecies

--- ArchitectureTrivia/app/models/question.rb
+++ ArchitectureTrivia/app/models/question.rb
@@ -1,4 +1,20 @@
 class Question < ApplicationRecord
     has_many :games 
     has_many :users, through: :games
+
+
+    # indecies = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    # def randomize 
+    #     num = indecies.sample 
+    #     indecies = (indecies - num)
+    #     return indecies 
+    # end 
+
+    # def show_random(n) 
+
+    # end 
+
 end
+
+
--- ArchitectureTrivia/config/routes.rb
+++ ArchitectureTrivia/config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/questions", to: "questions#index", as: "questions" 
+  get "/questions/:id", to: "questions#show", as: "question"
+
   
   get "/users", to: "user#index", as: "users"
   get "/users", to: "user#new", as: "new_user" 
