59cce7867f073f4dbf8763c2cc061a22722563e8
saazim1123 <saazim1123@gmail.com>
2019-09-10 10:55:12 +0100

    Dialogue Box and Username on its way

--- ArchitectureTrivia/mod2js.js
+++ ArchitectureTrivia/mod2js.js
@@ -1,9 +1,13 @@
-const alert = () => {
-    this.show = function(){
-        let dialogue = document.getElementById('')
-    }
-
-    this.closer = function(){
-
-    }
+var indecies = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+function pickRandom(indecies) {
+   let rand = indecies[Math.floor(Math.random() * indecies.length)];
+   indecies = indecies.splice(indecies[rand], 1)
+   fetchRandom(rand)
+}
+function fetchRandom(n) {
+   return fetch(`http://localhost:3000/questions/${n}`)
+   .then(resp => resp.json())
+   .then(showQuestion)
+}
+function showQuestion() {
 }
